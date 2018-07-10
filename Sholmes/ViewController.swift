6c838b2f2c2f4afd7d60e50a3967b5dc7a97ad5a
Brett <bratt.neumayer@gmail.com>
2018-07-10 22:47:17 +0100

    Initial version
    Only import of VisualRecognitionV3

--- Sholmes/ViewController.swift
+++ Sholmes/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import VisualRecognitionV3
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("1")
+        let visualRecognition = VisualRecognition(version: "2018-07-01",apiKey: sheli)
+        
         // Do any additional setup after loading the view, typically from a nib.
+        print("2")
+//        visualRecognition.
+//        visualRecognition.serviceURL = "https://gateway.watsonplatform.net/visual-recognition/api"
+        print(visualRecognition.serviceURL)
+        
+        
+        visualRecognition.classify(image: #imageLiteral(resourceName: "something.png")) { (images) in
+            
+            print(images)
+            print("a")
+            guard images.images.first?.classifiers != nil else {return}
+            print("b")
+            for a in images.images.first!.classifiers{
+                
+                print("c")
+                print(a.classifierID, a.name)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
